cd05db07b838290a6d5a335a85fc49814c9930f8
Nathan J. Morris | 莫南森 <59628824+pownathan77@users.noreply.github.com>
2021-03-25 20:21:21 -0400

    Update login.php

--- login.php
+++ login.php
@@ -3,18 +3,24 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$email = json_decode($_POST["email"], FALSE);
+$userpassword = json_decode($_POST["userPassword"], FALSE);
+
 $username = 'root';
 $password = 'Panther$';
 $databasename = 'ecommerce';
 $con=mysqli_connect("localhost",$username,$password,$databaseName);
 
-$query = check_login_info(accountid);
+$call = "CALL usp_GetLoginInfo('".$email."', '".$userpassword."');"
 $sqlsearch = $con->query($query) or die($con->error);
 
 $rows = array();
   while($r = $sqlsearch->fetch_assoc()) {
     $rows[] = $r;
   }
+
+header('Content-type: application/json');
+header('Access-Control-Allow-Origin: *');
 
 print json_encode($rows);
 ?>
