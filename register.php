4b0ff85a0bb9ab8b3e59ee6343f1fd74a69f6e18
Nathan J. Morris | 莫南森 <59628824+pownathan77@users.noreply.github.com>
2021-04-03 01:56:49 -0400

    d
    d

---
+++ register.php
@@ -0,0 +1,50 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (isset($_POST["email"])) {
+$email = $_POST["email"];
+}
+else
+{
+        echo "email variable is empty, or could not be accessed\n";
+}
+if (isset($_POST["userPassword"])) {
+$userpassword = $_POST["userPassword"];
+}
+else
+{
+        echo "variable is empty, or could not be accessed\n";
+}
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$username = $_POST["username"];
+$phone = $_POST["phone"];
+$taxexempt = $_POST["taxExempt"];
+
+$username = 'root';
+$password = 'Panther$';
+$databaseName = 'ecommerce';
+$con=mysqli_connect("localhost",$username,$password,$databaseName);
+
+$query = "CALL usp_Register('".$fname."', '".$lname."', '".$username."', '".$email."', '".$userpassword."', '".$phone."', '".$taxexempt."')";
+$sqlsearch = $con->query($query) or die($con->error);
+
+$rows = array();
+  while($r = $sqlsearch->fetch_assoc()) {
+    $rows['fname'] = $r['fname'];
+    $rows['lname'] = $r['lname'];
+    $rows['username'] = $r['username'];
+    $rows['email'] = $r['email'];
+    $rows['userPassword'] = $r['userPassword'];
+    $rows['phone'] = $r['phone'];
+    $rows['taxExempt'] = $r['taxExempt'];
+  }
+
+header('Content-Type: application/json; charset=UTL-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, HEAD');
+header('Access-Control-Allow-Headers: X-Requested-With');
+print json_encode($rows);
+?>
